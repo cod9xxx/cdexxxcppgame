f9e75787e5f0b2ac2977f6c62f4fe8fd5bdf8027
codexxx <egorgradov10@gmail.com>
2021-07-31 11:55:43 +0300

    Added build in repository and adde some callbacks in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,82 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
-
 #include <iostream>
+
+using namespace std;
+
+/*Making global variables for window size*/
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+/*Making window size callback*/
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX/2, g_windowSizeY);
+}
+
+/*Making window close if pressed Esc*/
+void glfwKeyClickCallback (GLFWwindow* pWindow, int key, int scancode,int action, int mode) {
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+
+}
 
 int main(void)
 {
-    GLFWwindow* window;
+    setlocale(LC_ALL, "Russian");
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        cout << "GLFW didn't init" << endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "WoT2d", nullptr, nullptr);
+
+    /*Debug window*/
+    if (!pWindow)
     {
+        cout << "glfwCreateWindow failed. GLFW couldn't create window";
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    /*Register window size in programm*/
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
 
+    /*Register key Esc callback*/
+    glfwSetKeyCallback(pWindow, glfwKeyClickCallback);
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+
+    /*Initialize GLAD*/
     if (!gladLoadGL()) {
-        std::cout << "Can't load glad!" << std::endl;
+        cout << "GLAD didn't load" << endl;
         return -1;
     }
 
     glClearColor(1, 0, 0, 1);
 
-    std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+    cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
